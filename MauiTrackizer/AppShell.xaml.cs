589d6d8d41c9d27f406c3fe831fe480641c18b9e
chsakell <chsakell@gmail.com>
2023-05-23 21:47:02 +0300

    Add project files.

---
+++ MauiTrackizer/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiTrackizer;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiTrackizer/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiTrackizer;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
