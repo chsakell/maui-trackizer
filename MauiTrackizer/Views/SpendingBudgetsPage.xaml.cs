fa89529bdbd5e3dbfd223b9ca59270bf0eaf7701
chsakell <chsakell@gmail.com>
2023-06-04 21:21:09 +0300

    feat(budgets): start page structure

--- MauiTrackizer/Views/SpendingBudgetsPage.xaml.cs
+++ MauiTrackizer/Views/SpendingBudgetsPage.xaml.cs
@@ -5,5 +5,12 @@ public partial class SpendingBudgetsPage : ContentPage
 	public SpendingBudgetsPage()
 	{
 		InitializeComponent();
+
+		BindingContext = new SpendingBudgetsPageViewModel();
 	}
+}
+
+public class SpendingBudgetsPageViewModel
+{
+	public string Title { get; set; } = "Spending & Budgets";
 }
