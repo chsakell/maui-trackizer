5cc436a808e3294da6c7cceaeb0013cccc68122a
chsakell <chsakell@gmail.com>
2023-06-05 19:02:35 +0300

    feat(budgets): complete page

--- MauiTrackizer/Views/SpendingBudgetsPage.xaml.cs
+++ MauiTrackizer/Views/SpendingBudgetsPage.xaml.cs
@@ -1,3 +1,8 @@
+using LiveChartsCore.Measure;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore;
+using SkiaSharp;
+
 namespace MauiTrackizer.Views;
 
 public partial class SpendingBudgetsPage : ContentPage
@@ -13,4 +18,19 @@ public partial class SpendingBudgetsPage : ContentPage
 public class SpendingBudgetsPageViewModel
 {
 	public string Title { get; set; } = "Spending & Budgets";
+    public IEnumerable<ISeries> DegreesGaugePieSeries { get; set; }
+    public SpendingBudgetsPageViewModel()
+    {
+
+        DegreesGaugePieSeries = new GaugeBuilder()
+        //.WithLabelsSize(50)
+        .WithInnerRadius(230)
+        .WithBackgroundInnerRadius(230)
+        .WithLabelsSize(0)
+        .WithLabelsPosition(PolarLabelsPosition.ChartCenter)
+        .AddValue(20, "gauge value", new SKColor(0, 250, 217), SKColors.White)
+        .AddValue(50, "gauge value", new SKColor(255, 121, 102), SKColors.White)
+        .AddValue(85, "gauge value", new SKColor(94, 0, 245), SKColors.White)
+        .BuildSeries();
+    }
 }
