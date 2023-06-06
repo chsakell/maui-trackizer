5ada87589c709cc5eb688ff98ea424f7f857ef21
chsakell <chsakell@gmail.com>
2023-06-06 22:12:55 +0300

    feat(cards page): add cards carousel

--- MauiTrackizer/Views/CreditCardsPage.xaml.cs
+++ MauiTrackizer/Views/CreditCardsPage.xaml.cs
@@ -5,5 +5,36 @@ public partial class CreditCardsPage : ContentPage
 	public CreditCardsPage()
 	{
 		InitializeComponent();
-	}
+
+		var vm = new CreditCardsViewModel();
+		vm.Cards.Add(new Card()
+		{
+			Icon = "card_1.png"
+		});
+        vm.Cards.Add(new Card()
+        {
+            Icon = "card_2.png"
+        });
+        vm.Cards.Add(new Card()
+        {
+            Icon = "card_3.png"
+        });
+
+        BindingContext = vm;
+    }
+}
+
+public class CreditCardsViewModel
+{
+	public List<Card> Cards { get; set; }
+
+    public CreditCardsViewModel()
+    {
+        Cards = new List<Card>();
+    }
+}
+
+public class Card
+{
+	public string Icon { get; set; }
 }
